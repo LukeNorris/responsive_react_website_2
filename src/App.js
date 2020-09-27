cd4e074c2f4a4874735671b850f6a25eec17485a
Luke Norris <luke.mjn@gmail.com>
2020-09-27 22:19:51 +0200

    created all styles and data for homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/pages/Navbar/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/pages/Homepage/Home'
 
 function App() {
     return (
         <Router>
             <Navbar />
             <Switch>
+                <Route path='/' exact component={Home} />
 
             </Switch>
         </Router>
---
+++ src/components/pages/Homepage/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../../HeroSection/HeroSection';
+import { homeObjOne, homeObjTwo, homeObjThree,  homeObjFour } from './data'
+
+function Home() {
+    return (
+        <>
+            <HeroSection {...homeObjOne}/>
+            <HeroSection {...homeObjThree}/>
+            <HeroSection {...homeObjTwo}/>
+            <HeroSection {...homeObjFour}/>
+            
+        </>
+    )
+}
+
+export default Home
